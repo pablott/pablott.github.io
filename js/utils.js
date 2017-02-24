18321ddaaafd82abbc6bacb7a25318ba767cf8dc
Pablo Trabajos <pablo.trabajos.tamayo@gmail.com>
2017-02-24 02:20:35 +0000

    Moved js functions to js/utils.js

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+var title = new Vue({
+  el: '#title',
+  data: {
+    message: 'Hello Portfolio!'
+  }
+});
+var time = new Vue({
+  el: '#time',
+  data: {
+    message: 'You loaded this page on ' + new Date()
+  }
+});
